c29bf95d2c95a34c01bd75edc613810d1f4c232b
Maddy Ali <maddyoali@gmail.com>
2022-10-08 11:11:42 -0500

    Add setIndex, getIndex & gameController

--- script.js
+++ script.js
@@ -7,15 +7,31 @@ const playerFactory = (name, marker) => {
 
 // Manage state of gameboard
 const gameBoard = (() => {
-  const array = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
-  return { array };
+  const array = ["", "", "", "", "", "", "", "", ""];
+
+  const setIndex = (index, marker) => {
+    if (index > array.length) return;
+    array[index] = marker;
+  };
+
+  const getIndex = (index) => {
+    if (index > array.length) return;
+    return array[index];
+  };
+
+  return { setIndex, getIndex };
 })();
 
 // Manage view of game: interact with DOM & collect info to create players
 const displayController = (() => {
   const fieldElements = document.querySelectorAll(".field");
+
   fieldElements.forEach((field) => {
     field.addEventListener("click", (e) => {
+      // return if target fieldEl is played or if game is over
+      if (e.target.textContent !== "") return;
+      // tell gameController to play a round using fieldEl data index
+      gameController.playRound(e.target.dataset.index);
       updateGameboard();
     });
   });
@@ -23,7 +39,25 @@ const displayController = (() => {
   // Render contents of the gameboard array to the webpage.
   const updateGameboard = () => {
     for (let i = 0; i < fieldElements.length; i++) {
-      fieldElements[i].textContent = gameBoard.array[i];
+      fieldElements[i].textContent = gameBoard.getIndex(i);
     }
   };
+})();
+
+// Manage flow of game: mediator between all other modules/functions
+const gameController = (() => {
+  const player1 = playerFactory("Player 1", "X");
+  const player2 = playerFactory("Player 2", "O");
+  let round = 1;
+
+  const playRound = (index) => {
+    gameBoard.setIndex(index, getPlayerMarker());
+    round++;
+  };
+
+  const getPlayerMarker = () => {
+    return round % 2 === 1 ? player1.marker : player2.marker;
+  };
+
+  return { playRound };
 })();
